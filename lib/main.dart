e867d05bc7aa62110a26279065f88b87a41db944
nazimfeni <mdnazimuddin77@gmail.com>
2023-09-29 20:30:27 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveApp(
+      builder: (context) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          home: HomePage(),
+        );
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  var myItems = [
+    {
+      "img": "images/lense.png",
+    },
+    {
+      "img": "images/lense.png",
+    },
+    {
+      "img": "images/lense.png",
+    },
+    {
+      "img": "images/lense.png",
+    },
+    {
+      "img": "images/lense.png",
+    },
+    {
+      "img": "images/lense.png",
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    ButtonStyle buttonStyle =
+        ElevatedButton.styleFrom(minimumSize: Size(double.infinity, 60));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          final isLandscape = orientation == Orientation.landscape;
+          final crossAxisCount = isLandscape ? 3 : 3;
+
+          return isLandscape
+              ? Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: ClipOval(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.width,
+                          child: Image.asset('images/camera.jpg',
+                              fit: BoxFit.cover),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Text(
+                              "John Doe",
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                            child: Text(
+                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo tincidunt augue Contrary to popular belief, simply random text.'),
+                          ),
+                          Expanded(
+                            child: GridView.builder(
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: crossAxisCount,
+                                crossAxisSpacing: 0.8,
+                                childAspectRatio: isLandscape ? 1.1 : .8,
+                              ),
+                              itemCount: myItems.length,
+                              itemBuilder: (context, index) {
+                                return GestureDetector(
+                                  child: Container(
+                                    margin: EdgeInsets.all(5),
+                                    width: double.infinity,
+                                    height: isLandscape ? 100 : 150,
+                                    child: Image.asset(
+                                      myItems[index]['img']!,
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              : Column(
+                  children: [
+                    ClipOval(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.width,
+                        child:
+                            Image.asset('images/camera.jpg', fit: BoxFit.cover),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        "John Doe",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                      child: Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo tincidunt augue Contrary to popular belief, simply random text.'),
+                    ),
+                    Expanded(
+                      child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: crossAxisCount,
+                          crossAxisSpacing: 0.8,
+                          childAspectRatio: isLandscape ? 0.9 : 1.1,
+                        ),
+                        itemCount: myItems.length,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            child: Container(
+                              margin: EdgeInsets.all(5),
+                              width: double.infinity,
+                              height: isLandscape ? 100 : 150,
+                              child: Image.asset(
+                                myItems[index]['img']!,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+        },
+      ),
+    );
+  }
+}
